b598f32656f327be1d45e818675457e9cc3b2e9a
Garrett Koller <gkoller@catalist.us>
2017-01-06 13:24:19 -0500

    Added Grunt config files

---
+++ grunt/imagemin.js
@@ -0,0 +1,11 @@
+module.exports = {
+    all: {
+        files: [{
+            expand: true,
+            cwd: 'src/',
+            src: ['images/*.{png,jpg,gif}'],
+            dest: 'dist/'
+        }]
+    }
+};
+
